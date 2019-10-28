49850f09162859e90cb2656fe09559df295a5d64
Angelika Baluta <ametist3g@gmail.com>
2019-10-28 17:01:21 +0200

    hosted project

---
+++ README.txt
@@ -0,0 +1,13 @@
+Slot machine simulation web-app.
+
+Slot machine page has following interface elements: 
+1. reels 
+2. pay-table 
+3. balance indicator (text-box) 
+4. SPIN button 
+5. Debug area
+
+It is possible use the slot machine in two modes: random and fixed. In case of random mode, the reels must land random positions. For fixed mode, the player must able enter two parameters for each reel.
+For enable debug mode you should put on the Debug switcher.
+
+Mobile design layout is available to use.
---
+++ precache-manifest.52c798d09d3f6dcb3102c0a95041a53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30907d81c88935c5afa7f737ae968a9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad0fa192acb1e16a94f8",
+    "url": "/static/css/main.16b34efd.chunk.css"
+  },
+  {
+    "revision": "7e9ba88b106055fa47a2",
+    "url": "/static/js/2.8f3372b2.chunk.js"
+  },
+  {
+    "revision": "ad0fa192acb1e16a94f8",
+    "url": "/static/js/main.23792f8d.chunk.js"
+  },
+  {
+    "revision": "48c436fc9779c813da33",
+    "url": "/static/js/runtime-main.82f1c925.js"
+  },
+  {
+    "revision": "a52c1a242ea5272e9fb0421686386910",
+    "url": "/static/media/symbols.a52c1a24.svg"
+  }
+]);
